57456f3b626f5c0036686dcdd9706a94d2eb9e59
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 22:05:05 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Solicite ao usuário que digite a sua senha (uma string). Exiba "Acesso permitido" caso a senha digitada seja 1234abcd, senão exiba "Acesso negado".
+
+Console.Clear();
+string senha;
+ 
+Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+senha = Convert.ToString(Console.ReadLine())!;
+ 
+if (senha != Convert.ToString("1234abcd"))
+{
+    Console.WriteLine("Acesso negado");
+}
+else
+{
+    Console.WriteLine("Acesso permitido");
+}
